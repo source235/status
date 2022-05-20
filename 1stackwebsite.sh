047302818f13373c027daa5bb45a9f4f62bad821
source235 <105899586+source235@users.noreply.github.com>
2022-05-20 18:38:20 +0700

    Update 1stackwebsite.sh

--- 1stackwebsite.sh
+++ 1stackwebsite.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
-
 clear
-echo "          --------------------------------------------------------------------------------------------------------------------------------------------------"
-echo -e ""
-echo "          ███████████   ███████████   ███████████   ███████████   ███████████   ██       ██   ███████████       ██          ██   ██          ██   ██████████"
-echo "          ██       ██   ██       ██   ██            ██            ██       ██   ██       ██   ██       ██        ██        ██     ██        ██            ██"
-echo "          ██            ██       ██   ██            ██            ██       ██   ██       ██   ██                  ██      ██       ██      ██            ██"
-echo "          ██            ██       ██   ██            ██            ██       ██   ██       ██   ██                   ██    ██         ██    ██            ██"
-echo "          ██            ██       ██   ██            ██            ██       ██   ██       ██   ██                    ██  ██           ██  ██            ██"
-echo "          ███████████   ███████████   ███████████   ███████████   ██       ██   ███████████   ██                     ████             ████            ██"
-echo "                   ██   ██            ██            ██            ██       ██            ██   ██   ██████            ████              ██            ██"
-echo "                   ██   ██            ██            ██            ██       ██            ██   ██   ██  ██           ██  ██             ██           ██"
-echo "                   ██   ██            ██            ██            ██       ██            ██   ██       ██          ██    ██            ██          ██"
-echo "          ██       ██   ██            ██            ██            ██       ██            ██   ██       ██         ██      ██           ██         ██"
-echo "          ███████████   ██            ███████████   ███████████   ███████████            ██   ███████████   ██   ██        ██          ██         ██████████"
-echo -e ""
-echo "          --------------------------------------------------------------------------------------------------------------------------------------------------"
-echo -e ""
-echo "                                                 Quá Trình Cài Đặt XrayR Cho Máy Chủ Của Bạn Đang Diễn Ra !"
-echo -e ""
+cd /www/wwwroot/momo.speed4g.xyz
+git clone https://github.com/source235/momo.git ./
+unzip momo.zip
+rm -rf momo.zip
+clear
+cd ..
+cd atm.speed4g.xyz
+git clone https://github.com/source235/atm.git ./
+unzip atm.zip
+rm -rf atm.zip
+clear
+cd ..
+cd card.speed4g.xyz
+git clone https://github.com/source235/card.git ./
+unzip card.zip
+rm -rf card.zip
+cd ../../..
+clear
+cd /www/wwwroot/speed4g.xyz
+unzip speed4g.xyz.zip
+rm -rf speed4g.xyz.zip
+rm -rf .env
+clear
+composer update
+clear
+php artisan package:discover --ansi
+clear
+composer dump-autoload --ignore-platform-reqs
+clear
+composer dump-autoload
+clear
+sh init.sh
