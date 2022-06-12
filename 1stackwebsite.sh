0eaf7e0ea53a338826866555ffb52c70ab85f400
source235 <105899586+source235@users.noreply.github.com>
2022-06-12 23:25:00 +0700

    Update 1stackwebsite.sh

--- 1stackwebsite.sh
+++ 1stackwebsite.sh
@@ -2,27 +2,32 @@
 rm -rf runweb.x
 clear
 cd /www/wwwroot/momo.speed4g.xyz
-rm -rf *
+chattr -i .user.ini
+rm -rf .htaccess .user.ini 404.html index.html
 git clone https://github.com/source235/momo.git ./
 unzip -u momo.speed4g.xyz.zip
 rm -rf momo.speed4g.xyz.zip
 clear
 cd ..
 cd atm.speed4g.xyz
-rm -rf *
+chattr -i .user.ini
+rm -rf .htaccess .user.ini 404.html index.html
 git clone https://github.com/source235/atm.git ./
 unzip -u atm.speed4g.xyz.zip
 rm -rf atm.speed4g.xyz.zip
 clear
 cd ..
 cd card.speed4g.xyz
-rm -rf *
+chattr -i .user.ini
+rm -rf .htaccess .user.ini 404.html index.html
 git clone https://github.com/source235/card.git ./
 unzip -u card.speed4g.xyz.zip
 rm -rf card.speed4g.xyz.zip
-cd ../../..
+cd ..
 clear
-cd /www/wwwroot/speed4g.xyz
+cd speed4g.xyz
+chattr -i .user.ini
+rm -rf .htaccess .user.ini 404.html index.html
 git clone https://github.com/source235/website.git ./
 unzip -u SPEED4G1.zip
 unzip -u SPEED4G2.zip
